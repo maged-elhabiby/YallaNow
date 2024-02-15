705bb62d7611eb446b9fa91e963bce4e211cead3
Marshal Kalynchuk <marshalkalynchuk1@gmail.com>
2024-02-15 15:46:19 -0700

    added recombee config file

---
+++ services/recombeeservice/src/main/java/org/yallanow/recombeeservice/config/RecombeeConfiguration.java
@@ -0,0 +1,37 @@
+package org.yallanow.recombeeservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import com.recombee.api_client.util.Region;
+
+@Configuration
+@ConfigurationProperties(prefix = "recombee")
+public class RecombeeConfiguration {
+    private String databaseId;
+    private String secretToken;
+    private Region region;
+
+    public String getDatabaseId() {
+        return databaseId;
+    }
+
+    public void setDatabaseId(String databaseId) {
+        this.databaseId = databaseId;
+    }
+
+    public String getSecretToken() {
+        return secretToken;
+    }
+
+    public void setSecretToken(String secretToken) {
+        this.secretToken = secretToken;
+    }
+
+    public Region getRegion() {
+        return region;
+    }
+
+    public void setRegion(Region region) {
+        this.region = region;
+    }
+}
