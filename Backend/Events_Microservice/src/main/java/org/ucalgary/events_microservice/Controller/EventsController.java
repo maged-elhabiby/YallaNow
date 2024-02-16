b1d4f80c163460475e02f3ee66b538be4a438e5a
AbdelrahmanBekhit <abdelrahman.bekhit@ucalgary.ca>
2024-02-16 16:47:35 -0700

    .

--- Backend/Events_Microservice/src/main/java/org/ucalgary/events_microservice/Controller/EventsController.java
+++ Backend/Events_Microservice/src/main/java/org/ucalgary/events_microservice/Controller/EventsController.java
@@ -1,5 +1,5 @@
 package org.ucalgary.events_microservice.Controller;
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.ucalgary.events_microservice.DTO.EventDTO;
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RequestParam;
 
 
 
--- Backend/Events_Microservice/src/main/java/org/ucalgary/events_microservice/Entity/eventsEntity.java
+++ Backend/Events_Microservice/src/main/java/org/ucalgary/events_microservice/Entity/eventsEntity.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import org.ucalgary.events_microservice.DTO.EventDTO;
+// import org.ucalgary.events_microservice.DTO.EventDTO;
 
 @Entity
 @Table(name = "event_table")
