0c2054c0c7c84a7b0be10731e43b3d2224ed15b6
Marshal Kalynchuk <marshalkalynchuk1@gmail.com>
2024-03-10 21:37:41 -0600

    Added basic helper methods to recombee client

--- services/feedservice/src/main/java/org/yallanow/feedservice/client/RecombeeClientImpl.java
+++ services/feedservice/src/main/java/org/yallanow/feedservice/client/RecombeeClientImpl.java
@@ -1,6 +1,40 @@
 package org.yallanow.feedservice.client;
 
+import org.yallanow.feedservice.exceptions.RecombeeClientException;
+import org.yallanow.feedservice.config.RecombeeConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-public class RecombeeClientImpl {
+import com.recombee.api_client.RecombeeClient;
+import com.recombee.api_client.api_requests.*;
+import com.recombee.api_client.exceptions.*;
+
+import java.util.Map;
+
+@Component
+public class RecombeeClientImpl implements RecombeeClientInterface {
+
+    private final RecombeeClient client;
+
+    @Autowired
+    public RecombeeClientImpl(RecombeeConfig recombeeConfig) {
+        this.client = new RecombeeClient(
+                recombeeConfig.getDatabaseId(),
+                recombeeConfig.getSecretToken()
+        ).setRegion(recombeeConfig.getRegion());
+    }
+
+    private void sendRequest(Request request) throws RecombeeClientException {
+        try {
+            client.send(request);
+        } catch (ApiException e) {
+            handleGenericApiException(e);
+        }
+    }
+
+    private void handleGenericApiException(ApiException e) throws RecombeeClientException {
+        throw new RecombeeClientException("An error occurred while communicating with Recombee", e);
+    }
+
 
 }
