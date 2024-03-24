96fd067d011f400e3cf96935accd08994775bc2e
Marshal <marshalkalynchuk1@gmail.com>
2024-03-23 22:01:48 -0600

    fixing docker file

--- services/feedservice/src/main/java/org/yallanow/feedservice/FeedServiceApplication.java
+++ services/feedservice/src/main/java/org/yallanow/feedservice/FeedServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FeedserviceApplication {
+public class FeedServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FeedserviceApplication.class, args);
+		SpringApplication.run(FeedServiceApplication.class, args);
 	}
 
 }
