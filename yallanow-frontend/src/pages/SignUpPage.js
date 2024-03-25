bb01f3620b500ed406cd10b7f87a12de0923fd21
maged-elhabiby <maged.elhabiby@ucalgary.ca>
2024-03-25 17:53:18 -0600

    SignUp Changes - Added Name

--- yallanow-frontend/src/pages/SignUpPage.js
+++ yallanow-frontend/src/pages/SignUpPage.js
@@ -1,8 +1,6 @@
-
 const SignUpPage = () => {
   return (
     <>
-
       <div className="mt-24 flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
@@ -17,6 +15,22 @@ const SignUpPage = () => {
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+                Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  autoComplete="name"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                 Email address
@@ -43,7 +57,7 @@ const SignUpPage = () => {
                 <input
                   id="password"
                   name="password"
-
+                  type="password"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -52,7 +66,7 @@ const SignUpPage = () => {
 
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                <label htmlFor="password-confirmation" className="block text-sm font-medium leading-6 text-gray-900">
                   Password Confirmation
                 </label>
               </div>
@@ -60,6 +74,7 @@ const SignUpPage = () => {
                 <input
                   id="password-confirmation"
                   name="password-confirmation"
+                  type="password"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -77,15 +92,15 @@ const SignUpPage = () => {
           </form>
 
           <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{' '}
+            Returning Member?{' '}
             <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-              Start a 14 day free trial
+              Sign In
             </a>
           </p>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default SignUpPage
+export default SignUpPage;
