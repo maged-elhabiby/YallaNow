2d0013e9b3381541a9777b5453289e5efb903ec9
Darkside224 <benzuidema6@gmail.com>
2024-03-24 22:12:11 -0600

    added a checkbox for private group

--- Frontend/src/components/groupCreation.js
+++ Frontend/src/components/groupCreation.js
@@ -19,6 +19,10 @@ function groupCreation() {
                             <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Group</label>
                             <input type="text" id="GroupName" class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Group Name" required />
                         </div>
+                        <div class="flex items-center mb-4">
+                            <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"/>
+                            <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Private Group</label>
+                        </div>
                         <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Group</button>
                     </form>
                 </div>
