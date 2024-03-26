b1b84adc1c6179ac7777d7156f9b006af336127f
Marshal Kalynchuk <marshalkalynchuk1@gmail.com>
2024-03-26 16:12:03 -0600

    removed issues

--- yallanow-frontend/src/pages/ExplorePage.js
+++ yallanow-frontend/src/pages/ExplorePage.js
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import EventCard from '../components/EventCard';
-import FeaturedEventsCarousel from '../components/FeaturedEvents';
 import feedService from '../api/feedService';
 
 const ExplorePage = () => {
   const userId = 10000;
   const initialiEventsCount = 20
   const [events, setEvents] = useState([]);
-  const [featuredEvents, setFeaturedEvents] = useState([]);
-  const [recommId, setRecommId] = useState(null);
+  const [homepageRecommId, setHomepageRecommId] = useState(null);
   const [loading, setLoading] = useState(false);
 
   const fetchHomepageEvents = async () => {
@@ -17,7 +15,7 @@ const ExplorePage = () => {
 
       const data = await feedService.getHomepageEvents(userId, initialiEventsCount);
       setEvents(data.recommendations ?? []);
-      setRecommId(data.recommId);
+      setHomepageRecommId(data.recommId);
     } catch (error) {
       console.error('Error fetching homepage events:', error);
     } finally {
@@ -25,26 +23,15 @@ const ExplorePage = () => {
     }
   };
 
-  const fetchFeaturedEvents = async () => {
-    try {
-      const data = await feedService.getPopularEvents(userId, 6);
-      setFeaturedEvents(data.recommendations ?? []);
-    } catch (error) {
-      console.error('Error fetching featured events:', error);
-    } finally {
-      setLoading(false);
-    }
-  }
-
   const fetchNextEvents = async () => {
-    if (!recommId || loading) return;
+    if (!homepageRecommId || loading) return;
     setLoading(true);
     try {
       console.log('Loading more events...');
       const count = 20; // Number of additional events to fetch
-      const data = await feedService.getNextEvents(count, recommId);
+      const data = await feedService.getNextEvents(count, homepageRecommId);
       setEvents((prevEvents) => [...prevEvents, ...data.recommendations]);
-      setRecommId(data.recommId);
+      setHomepageRecommId(data.recommId);
     } catch (error) {
       console.error('Error fetching next events:', error);
     } finally {
@@ -58,21 +45,19 @@ const ExplorePage = () => {
 
 
   return (
-    <div>
-      <div>
-        <FeaturedEventsCarousel featuredEvents={featuredEvents} />
-      </div>
-      <div className="container mx-auto py-8">
-        <h2 className="text-2xl font-semibold mb-6">Explore Events</h2>
+    <div className="mt-20">
+
+      <div className="container mx-auto py-8 px-4">
+        <h2 className="text-4xl font-semibold mb-6">Explore Events</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           {events.map((event) => (
-            <EventCard key={event.eventId} event={event} recommId={recommId} />
+            <EventCard key={event.eventId} event={event} recommId={homepageRecommId} />
           ))}
         </div>
 
         <div className="flex justify-center mt-10">
         {loading && <p>Loading more events...</p>}
-        {!loading && recommId && (
+        {!loading && homepageRecommId && (
 
           <button
             type="button"
