62e27cd5f2e96842115e858017d98f63a4b8239a
Youssef Mamoun <youssef.mamoun@ucalgary.ca>
2024-03-25 18:35:44 -0600

    added to firbase-config

--- yallanow-frontend/src/firebase-config.js
+++ yallanow-frontend/src/firebase-config.js
@@ -2,18 +2,93 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword , getIdToken, logout, sendPasswordResetEmail} from "firebase/auth";
+import{getDatabase, ref, set} from "firebase/database";
+
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAFU_zSpZWKznKiEiNqrNwsi-8JIr5XfhQ",
+  authDomain: "yallanow12.firebaseapp.com",
+  projectId: "yallanow12",
+  storageBucket: "yallanow12.appspot.com",
+  messagingSenderId: "463351798443",
+  appId: "1:463351798443:web:dc83ad7b3ebbf1e88f75ee",
+  measurementId: "G-77DNY9TMVN"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+const database = getDatabase();
 
-export const auth = getAuth(app);
+
+const Register = async (signup) => {
+  console.log("we are in register");
+  const { email, password } = signup;
+
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      console.log(userCredential);
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+
+    console.log("we are exiting register")
+}
+
+
+const Login = async (login) => {
+  console.log("we are in login");
+  const { email, password } = login;
+  await signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    console.log(userCredential)
+    const user = userCredential.user;
+    const idToken =  getIdToken(user);
+    console.log(idToken);
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+  console.log("we are exiting login");
+}
+
+
+const logout = async () => {
+  console.log("we are in logout");
+  await signOut(auth).then(() => {
+    console.log("we are exiting logout");
+  }).catch((error) => {
+    console.log(error);
+  });
+}
+
+const resetPassword = async (email) => {
+  console.log("we are in reset password");
+  await sendPasswordResetEmail(auth, email)
+  .then(() => {
+    console.log("we are exiting reset password");
+  }).catch((error) => {
+    console.log(error);
+  });
+}
+
+
+
+
+export {Register, Login, logout, resetPassword};
 export const googleProvider = new GoogleAuthProvider();
 
 // Youssef Add the Config
