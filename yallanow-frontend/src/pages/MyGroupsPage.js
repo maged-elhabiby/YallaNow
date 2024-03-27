fbcdebe9ed8e76c92d20690b7a6362f8102bd963
maged-elhabiby <maged.elhabiby@ucalgary.ca>
2024-03-27 13:17:13 -0600

    My Groups Page Done

--- yallanow-frontend/src/pages/MyGroupsPage.js
+++ yallanow-frontend/src/pages/MyGroupsPage.js
@@ -14,12 +14,12 @@ function MyGroupsPage() {
         ];
         setGroups(mockGroups);
       }, []);
-      
+
     return (
       <div className="px-6 py-24 sm:py-32 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
-          <p className="text-base font-semibold leading-7 text-pink-600">Get the help you need</p>
-          <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Your Groups</h2>
+          <p className="text-base font-semibold leading-7 text-pink-600">Welcome to Your</p>
+          <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Communities</h2>
           <p className="mt-6 text-lg leading-8 text-gray-600">
             All The Communities That You Have Joined
           </p>
