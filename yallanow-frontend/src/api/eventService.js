44ec72ec33eb231c1b72677b632d8e674e2852f4
Marshal <marshalkalynchuk1@gmail.com>
2024-03-27 13:24:23 -0600

    resolved naming and imports

--- yallanow-frontend/src/api/eventService.js
+++ yallanow-frontend/src/api/eventService.js
@@ -1,232 +1,124 @@
-import axios from "axios";
-const baseUrl = "http://localhost:8082/microservice/events/";
+import eventServiceApi from "./eventServiceApi";
+import imageService from "./imageService";
 
 const eventService = {
-  createEvent: async (eventRequest) => {
-    try {
-      const response = await axios.post(baseUrl + "AddEvent", eventRequest, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      return response.data;
-    } catch (error) {
-      if (error.response.status === 403) {
-        console.error("Access denied:", error);
-        throw new Error("Access denied");
-      } else if (error.response.status === 400) {
-        console.error("Bad request:", error);
-        throw new Error(error.response.data);
-      } else {
-        console.error("Error fetching events:", error);
-        throw error;
-      }
-    }
-  },
+    createEvent: async (eventData) => {
+        const imageData = await imageService.uploadImageByBase64(eventData.imageBase64);
+        eventData.imageId = imageData.id;
+        const eventImageUrl = imageData.imageUrl;
 
-  updateEvent: async (eventRequest) => {
-    try {
-      const response = await axios.post(baseUrl + "UpdateEvent", eventRequest, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      return response.data;
-    } catch (error) {
-      if (error.response.status === 403) {
-        console.error("Access denied:", error);
-        throw new Error("Access denied");
-      } else if (error.response.status === 400) {
-        console.error("Bad request:", error);
-        throw new Error(error.response.data);
-      } else {
-        console.error("Error fetching events:", error);
-        throw error;
-      }
-    }
-  },
+        const requestData = EventApi.formatEventForEventService(eventData);
+        const rawEvent = await eventServiceApi.createEvent(requestData);
+        const formattedEvent = EventApi.formatEventFromEventService(rawEvent);
+        formattedEvent.eventImageUrl = eventImageUrl;
 
-  getEvent: async (eventId) => {
-    try {
-      const response = await axios.get(baseUrl + "GetEvent/" + eventId);
-      return response.data;
-    } catch (error) {
-      if (error.response.status === 404) {
-        console.error("Event not found:", error);
-        throw new Error("Event not found");
-      } else {
-        console.error("Error fetching events:", error);
-        throw error;
-      }
-    }
-  },
+        return formattedEvent;
+    },
 
-  getEventsForGroup: async (groupId) => {
-    try {
-      const response = await axios.get(baseUrl + "GetGroupEvent/" + groupId);
-      return response.data;
-    } catch (error) {
-      if (error.response.status === 404) {
-        console.error("Event not found:", error);
-        throw new Error("Event not found");
-      } else {
-        console.error("Error fetching events:", error);
-        throw error;
-      }
-    }
-  },
-
-  deleteEvent: async (eventId) => {
-    try {
-      const response = await axios.delete(baseUrl + "DeleteEvent/" + eventId);
-      return response.data;
-    } catch (error) {
-      if (error.response.status === 403) {
-        console.error("Access denied:", error);
-        throw new Error("Access denied");
-      } else if (error.response.status === 404) {
-        console.error("Event not found:", error);
-        throw new Error("Event not found");
-      } else {
-        console.error("Error fetching events:", error);
-        throw error;
-      }
-    }
-  },
-
-  getRsvpdUsersForEvent: async (eventId) => {
-    try {
-      const response = await axios.get(
-        baseUrl + "GetAllEventParticipants/" + eventId
-      );
-      return response.data;
-    } catch (error) {
-      console.error("Error fetching events:", error);
-      throw error;
-    }
-  },
-
-  getUserRsvpdEvents: async (userId) => {
-    try {
-      const response = await axios.get(baseUrl + "GetAllUserEvents/");
-      return (
-        response.data.map((event) => eventService.formatEventForApp(event)) ||
-        []
-      );
-    } catch (error) {
-      if (error.response.status === 404) {
-        console.error("Event not found:", error);
-        throw new Error("Event not found");
-      } else {
-        console.error("Error fetching events:", error);
-        throw error;
-      }
-    }
-  },
-
-  // remove partitcipant status from event
-  unRsvpUserFromEvent: async (eventId) => {
-    try {
-      const response = await axios.delete(
-        baseUrl + "DeleteParticipant/" + eventId
-      );
-      return true; // Indicate that the participant was successfully removed
-    } catch (error) {
-      if (error.response.status === 404) {
-        console.error("Participant not found:", error);
-        throw new Error("User not part of Event");
-      } else {
-        console.error("Error removing participant from event:", error);
-        return false; // Indicate that the participant was not removed
-      }
-    }
-  },
-
-  // get participant status from event
-
-  isUserRsvpdToEvent: async (eventId) => {
-    try {
-      const response = await axios.get(
-        baseUrl + "GetParticipantStatus/" + eventId
-      );
-      console.log(
-        `RSVP status for user ${userId} in event ${eventId}:`,
-        response.data
-      );
-      return true;
-    } catch (error) {
-      if (error.response.status === 404) {
-        console.error("Member is not registered in Event.", error);
-        return false;
-      } else {
-        console.error("Error fetching RSVP status:", error);
-        return false; // Assume the user has not RSVP'd
-      }
-    }
-  },
-
-  // Add participant to event
-  rsvpUserToEvent: async (eventId) => {
-    const request = {
-      userId: null,
-      eventId: eventId,
-      participantStatus: "Attending",
-    };
-
-    try {
-      const response = await axios.post(baseUrl + "AddParticipant", request, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      console.log("Participant added successfully");
-      return true; // Indicate that the participant was successfully added
-    } catch (error) {
-      if (error.response.status === 422) {
-        console.error("Maximum capacity reached.");
-        throw new Error("Maximum capacity reached.");
-      } else if (error.response.status === 404) {
-        console.error("Event not found:", error);
-        throw new Error("Event not found");
-      } else {
-        console.error("Error adding participant to event:", error);
-        return false; // Indicate that the participant was not added
-      }
-    }
-  },
-
-  updateRsvpStatus: async (eventId, status) => {
-    const request = {
-      userId: null,
-      eventId: eventId,
-      participantStatus: status,
-    };
-
-    try {
-      const response = await axios.post(
-        baseUrl + "UpdateParticipant",
-        request,
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
+    updateEvent: async (eventData) => {
+        let eventImageUrl = null;
+        if (eventData.imageBase64) {
+            const imageData = await imageService.uploadImageByBase64(eventData.imageBase64);
+            eventData.imageId = imageData.id;
+            eventImageUrl = imageData.imageUrl;
+        } else {
+            eventImageUrl = await imageService.getImageUrlById(eventData.imageId);
         }
-      );
-      console.log("Participant status updated successfully");
-      return true; // Indicate that the participant status was successfully updated
-    } catch (error) {
-      if (error.response.status === 404) {
-        console.error("Participant not found:", error);
-        throw new Error("Participant not found");
-      } else if (error.response.status === 422) {
-        console.error("Maximum capacity reached.");
-        throw new Error("Maximum capacity reached.");
-      } else {
-        console.error("Error updating participant status:", error);
-        return false; // Indicate that the participant status was not updated
-      }
-    }
-  },
+
+        const requestData = EventApi.formatEventForEventService(eventData);
+        const rawEvent = await eventServiceApi.updateEvent(requestData);
+        const formattedEvent = EventApi.formatEventFromEventService(rawEvent);
+        formattedEvent.eventImageUrl = eventImageUrl;
+
+        return formattedEvent;
+    },
+
+    getEvent: async (eventId) => {
+        const rawEvent = await eventServiceApi.getEvent(eventId);
+        const formattedEvent = EventApi.formatEventFromEventService(rawEvent);
+        formattedEvent.eventImageUrl = await imageService.getImageUrlById(formattedEvent.imageId);
+
+        return formattedEvent;
+    },
+
+    getEventsForGroup: async (groupId) => {
+        const rawEvents = await eventServiceApi.getEventsForGroup(groupId);
+        const eventsWithImages = await Promise.all(rawEvents.map(async (event) => {
+            const formattedEvent = EventApi.formatEventFromEventService(event);
+            formattedEvent.eventImageUrl = await imageService.getImageUrlById(event.imageId);
+            return formattedEvent;
+        }));
+
+        return eventsWithImages;
+    },
+
+    deleteEvent: async (eventId) => {
+        return await eventServiceApi.deleteEvent(eventId);
+    },
+
+    getRsvpdUsersForEvent: async (eventId) => {
+        return await eventServiceApi.getRsvpdUsersForEvent(eventId);
+    },
+
+    getUserRsvpdEvents: async (userId) => {
+        const rawEvents = await eventServiceApi.getUserRsvpdEvents(userId);
+        const eventsWithImages = await Promise.all(rawEvents.map(async (event) => {
+            const formattedEvent = EventApi.formatEventFromEventService(event);
+            formattedEvent.eventImageUrl = await imageService.getImageUrlById(event.imageId);
+            return formattedEvent;
+        }));
+
+        return eventsWithImages;
+    },
+
+    unRsvpUserFromEvent: async (userId, eventId) => {
+        return await eventServiceApi.unRsvpUserFromEvent(userId, eventId);
+    },
+
+    isUserRsvpdToEvent: async (userId, eventId) => {
+        return await eventServiceApi.isUserRsvpdToEvent(userId, eventId);
+    },
+
+    rsvpUserToEvent: async (userId, eventId) => {
+        return await eventServiceApi.rsvpUserToEvent(userId, eventId);
+    },
+
+    updateRsvpStatus: async (userId, eventId, status) => {
+        return await eventServiceApi.updateRsvpStatus(userId, eventId, status);
+    },
+
+    formatEventForEventService: (data) => {
+        return {
+            eventID: data.eventId,
+            groupID: data.groupId,
+            imageID: data.imageId,
+            eventTitle: data.eventTitle,
+            eventDescription: data.eventDescription,
+            location: data.location,
+            eventStartTime: data.eventStartTime,
+            eventEndTime: data.eventEndTime,
+            status: data.eventStatus,
+            capacity: data.eventCapacity,
+            count: data.eventAttendeeCount,
+        };
+    },
+
+    formatEventFromEventService: (data) => {
+        return {
+            eventId: data.eventID,
+            groupId: data.groupID,
+            imageId: data.imageID,
+            eventTitle: data.eventTitle,
+            eventDescription: data.eventDescription,
+            location: data.location,
+            eventStartTime: data.eventStartTime,
+            eventEndTime: data.eventEndTime,
+            eventStatus: data.status,
+            eventCapacity: data.capacity,
+            eventAttendeeCount: data.count,
+            eventImageUrl: null,
+        };
+    },
 };
 
 export default eventService;
